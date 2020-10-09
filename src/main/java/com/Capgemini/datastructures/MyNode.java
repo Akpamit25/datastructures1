693e077f9bb3eb3552f01156dea3c8a9506cdce7
Amit Kumar <amitnitsri25@gmail.com>
2020-10-09 08:51:59 +0530

    UC5 Added

--- src/main/java/com/Capgemini/datastructures/MyNode.java
+++ src/main/java/com/Capgemini/datastructures/MyNode.java
@@ -1,19 +1,28 @@
 package com.Capgemini.datastructures;
 
-public class MyNode<K> {
+public class MyNode<K> implements INode<K> {
 	private K key;
-	private MyNode<K> next;
-	
+	private INode next;
+
 	public MyNode(K key) {
-		this.key= key;
+		this.key = key;
+		this.next = null;
 	}
 
-	public MyNode getNext() {
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public INode<K> getNext() {
 		return next;
 	}
 
-	public void setNext(MyNode next) {
+	public void setNext(INode next) {
 		this.next = next;
 	}
-	
+
+	@Override
+	public K getKey() {
+		return this.key;
+	}
 }
--- src/test/java/com/Capgemini/datastructures/MyNodeTest.java
+++ src/test/java/com/Capgemini/datastructures/MyNodeTest.java
@@ -2,16 +2,16 @@ package com.Capgemini.datastructures;
 
 import org.junit.Assert;
 import org.junit.Test;
+
 public class MyNodeTest {
 	@Test
 	public void given3NumbersAddedToList() {
-	MyNode<Integer> myFirstNode = new MyNode<>(56);
-	MyNode<Integer> mySecondNode = new MyNode<>(30);
-	MyNode<Integer> myThirdNode = new MyNode<>(70);
-	myFirstNode.setNext(mySecondNode);
-	mySecondNode.setNext(myThirdNode);
-	boolean result = myFirstNode.getNext().equals(mySecondNode)&&
-					 mySecondNode.getNext().equals(myThirdNode);
-	Assert.assertTrue(result);
-}
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		myFirstNode.setNext(mySecondNode);
+		mySecondNode.setNext(myThirdNode);
+		boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+		Assert.assertTrue(result);
 	}
+}
